44d51f6ad9919c0bd62cee421186278c79e0039e
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2022-04-16 13:48:21 +0200

    Time Converter - Delete save to file and some changes

--- src/time_converter.py
+++ src/time_converter.py
@@ -5,9 +5,7 @@
 from typing import TypeVar
 from enum import Enum
 
-from file_reader import FileReader
-
-T = TypeVar('T', bound='TimeConverter')
+TimeConverterObject = TypeVar('TimeConverterObject', bound='TimeConverter')
 
 
 class Unit(Enum):
@@ -24,36 +22,36 @@ class TimeConverter:
     """
     This class converts time to minutes, seconds, milliseconds and microseconds.
     """
+
     __seconds: float = None
     """Stores the number of seconds."""
 
     def __init__(self, seconds: float = 0) -> None:
         """
         This constructor writes the given time value (in seconds).
+
         :param seconds: float | Variable for storing and converting time
         :return: None
         """
-
         self.set_time(seconds)
 
-    def set_time(self, value: float, unit: Unit = Unit.SEC) -> T:
+    def set_time(self, value: float, unit: Unit = Unit.SEC) -> TimeConverterObject:
         """
         This constructor writes the given time value.
+
         :param value: float | Variable for storing and converting time
         :param unit: Unit | What unit of time to use
         :return: None
         """
         if value < 0:
-            FileReader.append_error("The time in TimeConverter class cannot be negative.")
             raise ValueError("The time cannot be negative.")
-
         self.__seconds = value / unit.get_value()
-
         return self
 
     def as_minutes(self) -> float:
         """
         This method returns the time in minutes.
+
         :return: float | Number of minutes
         """
         return self.__seconds / 60
@@ -61,6 +59,7 @@ class TimeConverter:
     def as_seconds(self) -> float:
         """
         This method returns the time in seconds.
+
         :return: float | Number of seconds
         """
         return self.__seconds
@@ -68,6 +67,7 @@ class TimeConverter:
     def as_milliseconds(self) -> int:
         """
         This method returns the time in milliseconds.
+
         :return: int | Number of milliseconds
         """
         return int(self.__seconds * 1000)
@@ -75,6 +75,7 @@ class TimeConverter:
     def as_microseconds(self) -> int:
         """
         This method returns time in microseconds.
+
         :return: int | Number of microseconds
         """
         return int(self.__seconds * 1000000)
