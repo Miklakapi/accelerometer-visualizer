b5a33a97912d4cdf717585fd05460cfff250fd3c
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2022-04-16 15:53:45 +0200

    import from root v2

--- test/import_from_root.py
+++ test/import_from_root.py
@@ -3,6 +3,9 @@
 """Adds a path for importing between directories to work"""
 
 import sys
+from pathlib import Path
 
-sys.path.insert(1, sys.path[0][0:-4])
-sys.path.insert(1, sys.path[0][0:-4] + 'src')
+here = Path(__file__).resolve()
+
+sys.path.insert(1, str(here.parent.parent.absolute()))
+sys.path.insert(1, str(here.parent.parent.absolute()) + '/src')
